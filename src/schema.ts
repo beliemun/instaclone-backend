30dd9771fcb4dd00e1527a30a23418aecf477f5a
beliemun <beliemun@paran.com>
2021-09-14 22:40:07 +0900

    update

--- src/schema.ts
+++ src/schema.ts
@@ -4,4 +4,4 @@ const loadedTypes = loadFilesSync(`${__dirname}/**/*.typeDefs.*`);
 const loadedResolvers = loadFilesSync(`${__dirname}/**/*.resolvers.*`);
 
 export const typeDefs = mergeTypeDefs(loadedTypes);
-export const resolvers = mergeResolvers(loadedResolvers) as any;
+export const resolvers = mergeResolvers(loadedResolvers) as any; // [중요] any 타입으로 안해주면, 타입이 안맞아서 헤로쿠에서 푸시할 때 오류남
